249190e3af94e1555a03791a1a67a96410c90d74
Mgamerz <mgamerz@msn.com>
2025-07-05 17:14:10 -0600

    FallbackLink - support load balancing

--- ME3TweaksCore/Misc/FallbackLink.cs
+++ ME3TweaksCore/Misc/FallbackLink.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ME3TweaksCore.Misc
 {
@@ -12,6 +8,10 @@ namespace ME3TweaksCore.Misc
     /// </summary>
     public class FallbackLink
     {
+        /// <summary>
+        /// If this URL should randomly pick one of the two links to reduce load
+        /// </summary>
+        public bool LoadBalancing { get; init; }
         public string MainURL { get; init; }
         public string FallbackURL { get; init; }
 
@@ -22,8 +22,18 @@ namespace ME3TweaksCore.Misc
         public List<string> GetAllLinks()
         {
             var urls = new List<string>();
-            if (MainURL != null) urls.Add(MainURL);
-            if (FallbackURL != null) urls.Add(FallbackURL);
+            if (LoadBalancing && new Random().Next(2) == 0)
+            {
+                // Load balance - swap url order
+                if (FallbackURL != null) urls.Add(FallbackURL);
+                if (MainURL != null) urls.Add(MainURL);
+            }
+            else
+            {
+                if (MainURL != null) urls.Add(MainURL);
+                if (FallbackURL != null) urls.Add(FallbackURL);
+            }
+
             return urls;
         }
     }
